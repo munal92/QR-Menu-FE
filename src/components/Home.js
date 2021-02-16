8537dcb6d2adcff0dbe0f302398aba4761642a2c
Mucteba Fatih Unal <m_fattih@hotmail.com>
2021-02-16 15:40:47 -0800

    fixed small mobile view

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,22 +8,20 @@ const Home = () => {
   return (
     <>
       <div className="containerHeader">
-        <Container className=" p-5">
+        <Container>
           <Row className="titleHome">
             <Col lg={6} className="justify-content-center align-self-center">
-              <Row className="headerTitle justify-content-center text-center mb-2">
+              <Row className="headerTitle justify-content-center mb-2">
                 Create a QR menu for your restaurant
               </Row>
-              <Row className="headerSubTitle justify-content-center mb-5">
-                Quick.Simple.Free.
-              </Row>
-              <Row className="homeButtonCont justify-content-center mb-5">
+              <Row className="headerSubTitle  mb-5">Quick.Simple.Free.</Row>
+              <Row className="homeButtonCont  mb-5">
                 <Button href="#signupF" size="lg" variant="teal1">
                   Sign Up
                 </Button>{" "}
               </Row>
             </Col>
-            <Col lg={6} className="imageCol ">
+            <Col lg={6} className="imageCol pl-5">
               <Image src={phoneImg} fluid></Image>
             </Col>
           </Row>
